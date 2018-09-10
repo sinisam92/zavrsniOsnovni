6de732439d9adde7dd76117f795432d57302d507
sinisam92 <nikolapacariz@gmail.com>
2018-09-10 22:00:00 +0200

    Uradjen 1,2, 4 zadatak

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "vivify";
+$dbname = "sinisaBlog";
+
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+?>
